22beeab2d16cca40b49206bb0a4726ef8125f5fe
LeeJeongGi <apple4rhk@naver.com>
2022-08-08 23:38:59 +0900

    feat - authentication 기본적인 커스텀 사용 방법 숙지

--- server/login-custom-filter/src/main/java/com/sp/fc/web/CustomLoginApp.java
+++ server/login-custom-filter/src/main/java/com/sp/fc/web/CustomLoginApp.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CustomLoginApp {
-
-
     public static void main(String[] args) {
         SpringApplication.run(CustomLoginApp.class, args);
     }
